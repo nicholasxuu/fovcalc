b448b59974ce7ec8492b92045a460a938e8dc721
Nicholas Xu <shengfeng.xu@hotmail.com>
2021-05-08 17:12:03 +0800

    update dist page

--- build.sh
+++ build.sh
@@ -7,23 +7,20 @@ INPUT_JS=(
   fovcalc.js
 )
 
-mkdir -p build
+mkdir -p docs
 
-rm build/*
-rm ./index.html
-rm ./fovcalc.*
+rm docs/*
 
-#uglifyjs -b --warn "${INPUT_JS[@]}" > build/$JS_FILENAME &&
+#uglifyjs -b --warn "${INPUT_JS[@]}" > docs/$JS_FILENAME &&
   #echo $JS_FILENAME generated
-cp src/fovcalc.js build/$JS_FILENAME &&
+cp src/fovcalc.js docs/$JS_FILENAME &&
   echo $JS_FILENAME created
 
-cp src/fovcalc.css build/$CSS_FILENAME &&
+cp src/fovcalc.css docs/$CSS_FILENAME &&
   echo $CSS_FILENAME created
 
 FAVICON_B64=$(cat favicon.png | openssl base64 | tr -d '\n') && echo "Base64-encoded favicon"
 
-sed -e "s/fovcalc.css/$CSS_FILENAME/;s/fovcalc.js/$JS_FILENAME/;s|favicon.png|data:image/png;base64,$FAVICON_B64|;s|http://localhost:9933||" src/fovcalc.html > build/index.html &&
-  echo Filenames replaced in build/index.html
+sed -e "s/fovcalc.css/$CSS_FILENAME/;s/fovcalc.js/$JS_FILENAME/;s|favicon.png|data:image/png;base64,$FAVICON_B64|;s|http://localhost:9933||" src/fovcalc.html > docs/index.html &&
+  echo Filenames replaced in docs/index.html
 
-cp ./build/* ./
